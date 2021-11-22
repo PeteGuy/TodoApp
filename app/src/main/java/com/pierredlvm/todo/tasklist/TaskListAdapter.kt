8575375413ec16774f19c7e7811b032986462bae
Pierre <pierredelavillemontbazon@gmail.com>
2021-11-22 17:56:12 +0100

    Début  TP2

--- app/src/main/java/com/pierredlvm/todo/tasklist/TaskListAdapter.kt
+++ app/src/main/java/com/pierredlvm/todo/tasklist/TaskListAdapter.kt
@@ -1,32 +1,73 @@
 package com.pierredlvm.todo.tasklist
 
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.pierredlvm.todo.R
+import com.pierredlvm.todo.databinding.FragmentTaskListBinding
+import com.pierredlvm.todo.databinding.ItemTaskBinding
 
-class TaskListAdapter(private val taskList: List<String>) : RecyclerView.Adapter<TaskListAdapter.TaskViewHolder>()
+
+object TasksDiffCallback : DiffUtil.ItemCallback<Task>()
 {
-    inner class TaskViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        fun bind(taskTitle: String) {
+    override fun areItemsTheSame(oldItem: Task, newItem: Task): Boolean {
 
+
+
+            return oldItem.id == newItem.id;
+
+    }
+
+    override fun areContentsTheSame(oldItem: Task, newItem: Task): Boolean {
+
+        return (oldItem.title == newItem.title && oldItem.description == newItem.description);
+    }
+
+}
+
+/*interface TaskListListener
+{
+    fun onClickDelete(task:Task);
+}*/
+
+class TaskListAdapter() : ListAdapter<Task,TaskListAdapter.TaskViewHolder>(TasksDiffCallback)
+{
+    var onClickDelete: ((Task) -> Unit)? = null
+    private lateinit var binding: ItemTaskBinding;
+    inner class TaskViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+        fun bind(task:Task) {
+
+            val textView = binding.taskTitle;
+
+            textView.setText(task.title+"\n"+task.description);
+            binding.deleteButton.setOnClickListener { onClickDelete?.invoke(task) };
         }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TaskViewHolder {
 
-        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.item_task, parent, false);
+        //val itemView = LayoutInflater.from(parent.context).inflate(R.layout.item_task, parent, false);
+        binding = ItemTaskBinding.inflate(LayoutInflater.from(parent.context));
+        val itemView = binding.root;
         val taskViewHolder = TaskViewHolder(itemView);
+
         return taskViewHolder;
 
     }
 
     override fun onBindViewHolder(holder: TaskViewHolder, position: Int) {
-        holder.bind(taskList[position]);
+        holder.bind(currentList[position]);
+
+
+
+       // binding.deleteButton.setOnClickListener { onClickDelete?.invoke(currentList[position]) };
     }
 
-    override fun getItemCount(): Int {
-        return taskList.size;
-    }
+
 }
--- app/src/main/java/com/pierredlvm/todo/tasklist/TaskListFragment.kt
+++ app/src/main/java/com/pierredlvm/todo/tasklist/TaskListFragment.kt
@@ -1,34 +1,94 @@
 package com.pierredlvm.todo.tasklist
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.pierredlvm.todo.R
+import com.pierredlvm.todo.databinding.FragmentTaskListBinding
+import java.util.*
 
 class TaskListFragment : Fragment()
 {
-    private val taskList = listOf("Task 1", "Task 2", "Task 3")
+    /*val adapterListener = object : TaskListListener {
+        override fun onClickDelete(task: Task)
+        {
+
+        }
+    }*/
+
+    private lateinit var binding: FragmentTaskListBinding;
+    /*val adapterListener = object : TaskListListener{
+    override fun onClickAdd(task: Task) { taskList.add(taskList.size,Task(id = UUID.randomUUID().toString(), title = "Task ${taskList.size + 1}"));
+            recyclerView.adapter.submitList(taskList.toList());}*/
+
+    private val taskList = mutableListOf(
+        Task(id = "id_1", title = "Task 1", description = "description 1"),
+        Task(id = "id_2", title = "Task 2"),
+        Task(id = "id_3", title = "Task 3"));
+
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val rootView = inflater.inflate(R.layout.fragment_task_list, container, false)
 
-        //return super.onCreateView(inflater, container, savedInstanceState)
+        binding = FragmentTaskListBinding.inflate(inflater);
+        val rootView = binding.root;
+
+
         return rootView;
     }
 
 
+
+
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val recyclerView = view.findViewById<RecyclerView>(R.id.recycler_view)
+
+
+        val recyclerView = binding.recyclerView;//view.findViewById<RecyclerView>(R.id.recycler_view)
         recyclerView.layoutManager = LinearLayoutManager(activity);
-        recyclerView.adapter = TaskListAdapter(taskList);
+        val myAdapter = TaskListAdapter();
+
+        val taskDelete:(Task) -> Unit =
+            {task ->
+
+                taskList.remove(task);
+                /*for(i in 0..taskList.size-1)
+                {
+                    if(taskList[i].id == task.id)
+                    {
+                        taskList.removeAt(i);
+                    }
+                }*/
+
+                myAdapter.submitList(taskList.toList());
+
+
+            };
+
+
+
+        myAdapter.onClickDelete = taskDelete;
+        recyclerView.adapter  = myAdapter;
+
+        myAdapter.submitList(taskList.toList());
+
+        val addButton = binding.addTaskButton;//view.findViewById<FloatingActionButton>(R.id.addTaskButton);
+        addButton.setOnClickListener { taskList.add(Task(id = UUID.randomUUID().toString(), title = "Task ${taskList.size + 1}"));
+            myAdapter.submitList(taskList.toList())
+            Log.e("TaskListSize",taskList.size.toString());
+        }
+
+
+
     }
 }
